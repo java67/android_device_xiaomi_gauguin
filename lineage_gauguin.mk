035add34da556de84e7155f6fcfcd5dec8872534
baolong24 <1667375308@qq.com>
2021-02-16 20:48:55 +0800

    gauguin: it's gauguin

--- lineage_gauguin.mk
+++ lineage_gauguin.mk
@@ -1,11 +1,8 @@
 #
-# Copyright (C) 2020 Lineage OS
+# Copyright (C) 2021 The exTHmUI Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
-# ANXCamera
-$(call inherit-product, vendor/ANXCamera/config.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
@@ -14,41 +11,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from gauguin device
 $(call inherit-product, device/xiaomi/gauguin/device.mk)
 
-# Inherit some common Lineage stuff.
+# Inherit some common exTHmUI stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
-# Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_gauguin
 PRODUCT_DEVICE := gauguin
-PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := Redmi Note 9 Pro
-PRODUCT_MANUFACTURER := Redmi
-
-# PixelGApps
-TARGET_GAPPS_ARCH := arm64
-TARGET_INCLUDE_STOCK_ARCORE := true
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_SHIPS_SEPERATE_GAPPS_BUILD := true
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 2400
-TARGET_SCREEN_WIDTH := 1080
-
-#Device Maintainers
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    DEVICE_MAINTAINERS="NameLess"
-
-# Adds face unlock if package is available on ROM source.
-TARGET_SUPPORT_FACE_UNLOCK := true
-
-# Build info
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ1A.201205.010/6953398:user/release-keys
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-    ro.build.fingerprint=$(BUILD_FINGERPRINT)
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE=gauguin \
-    PRODUCT_NAME=gauguin \
-    PRIVATE_BUILD_DISC="redfin/redfin:11/RQ1A.201205.010/6953398:user/release-keys"
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi 10T Lite
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="gauguin-user 11 RKQ1.200826.002 20.2.3 release-keys"
+
+BUILD_FINGERPRINT := Redmi/gauguinpro/gauguin:11/RKQ1.200826.002/20.2.3:user/release-keys
